1cf8c489ae969eecb30b941d9311e37b3be9b21b
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-06-18 09:07:04 +0100

    Added comments for the book

--- Test/UnitTests/TestDataLayer/Ch09_SeedWithMirgation.cs
+++ Test/UnitTests/TestDataLayer/Ch09_SeedWithMirgation.cs
@@ -6,22 +6,46 @@ using Microsoft.EntityFrameworkCore;
 using Test.Chapter09Listings.SeedExample;
 using TestSupport.EfHelpers;
 using Xunit;
+using Xunit.Abstractions;
 using Xunit.Extensions.AssertExtensions;
 
 namespace Test.UnitTests.TestDataLayer
 {
     public class Ch09_SeedWithMirgation
     {
+        private ITestOutputHelper _output;
+
+        public Ch09_SeedWithMirgation(ITestOutputHelper output)
+        {
+            _output = output;
+        }
 
         [Fact]
-        public void TestSeedingOk()
+        public void TestSeedingOnMigrateOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptionsWithLogging<SeedExampleDbContext>(log => _output.WriteLine(log.ToString()));
+            using (var context = new SeedExampleDbContext(options))
+            {
+                //ATTEMPT
+                context.Database.Migrate();
+
+                //VERIFY
+                var projects = context.Projects.Include(x => x.ProjectManager).ToList();
+                projects.Count.ShouldEqual(2);
+                projects.Select(x => x.ProjectManager).All(x => x != null).ShouldBeTrue();
+            }
+        }
+
+        [Fact]
+        public void TestSeedingOnEnsureCreatedOk()
         {
             //SETUP
             var options = SqliteInMemory.CreateOptions<SeedExampleDbContext>();
             using (var context = new SeedExampleDbContext(options))
             {
                 //ATTEMPT
-                context.Database.Migrate();
+                context.Database.EnsureCreated();
 
                 //VERIFY
                 var projects = context.Projects.Include(x => x.ProjectManager).ToList();
