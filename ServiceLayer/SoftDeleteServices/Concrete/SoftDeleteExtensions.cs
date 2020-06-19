049629a800bb3b18a3371f76bd500f2094621881
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-06-19 14:03:06 +0100

    Started looking at cascade soft delete

---
+++ ServiceLayer/SoftDeleteServices/Concrete/SoftDeleteExtensions.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2020 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ServiceLayer.SoftDeleteServices.Concrete
+{
+    public static class SoftDeleteExtensions
+    {
+        public static void CascadeSoftDelete<TEntity>(this DbContext context, TEntity softDeleteThisEntity)
+            where TEntity : class
+        {
+            var startingPoint = context.Model.FindEntityType(typeof(TEntity));
+            if (startingPoint == null)
+                throw new ArgumentException($"The class {typeof(TEntity).Name} is not in the database.", nameof(softDeleteThisEntity));
+
+            var allEntities = context.Model
+                .GetEntityTypes().ToList();
+
+            void SetCascadeSoftDelete<T>(T principalInstance, IEntityType principal, int cascadeLevel)
+            {
+                var dependents = allEntities.SelectMany(x => x.GetForeignKeys()
+                    .Where(y => y.PrincipalEntityType == principal)).Distinct().ToList();
+
+                foreach (var foreignKey in dependents)
+                {
+                    var navProperty = foreignKey.PrincipalToDependent.PropertyInfo;
+                    var principalNavs = context.Entry(principalInstance)
+                        .Metadata.GetNavigations()
+                        .ToList();
+                }
+            }
+
+            SetCascadeSoftDelete(softDeleteThisEntity, startingPoint, 0);
+
+
+        }
+
+
+    }
+}
---
+++ Test/UnitTests/TestDataLayer/Ch11_CascadeSoftDelete.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2020 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System.Linq;
+using DataLayer.EfCode;
+using Microsoft.EntityFrameworkCore;
+using ServiceLayer.SoftDeleteServices.Concrete;
+using Test.TestHelpers;
+using TestSupport.EfHelpers;
+using Xunit;
+
+namespace Test.UnitTests.TestDataLayer
+{
+    public class Ch11_CascadeSoftDelete
+    {
+        [Fact]
+        public void TestSeedingOnMigrateSqliteOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<EfCoreContext>();
+            using (var context = new EfCoreContext(options))
+            {
+                context.Database.EnsureCreated();
+                var books = context.SeedDatabaseFourBooks();
+
+                //ATTEMPT
+                //context.CascadeSoftDelete(books.Last());
+
+                //VERIFY
+            }
+        }
+    }
+}
