e82d4a211599ea3430afc4ab1e08d46eed488614
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-08-19 19:53:31 +0100

    Fixed bug around UserId

---
+++ BookApp.ServiceLayer.EfCoreSql.Orders/UserIdServices/UserIdService.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2020 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System;
+using BookApp.Infrastructure.Orders.BizLogic.BasketServices;
+using BookApp.Persistence.EfCoreSql.Orders;
+using BookApp.ServiceLayer.EfCoreSql.Orders.CheckoutServices.Concrete;
+using Microsoft.AspNetCore.Http;
+
+namespace BookApp.ServiceLayer.EfCoreSql.Orders.UserIdServices
+{
+    public class UserIdService : IUserIdService
+    {
+        private readonly IHttpContextAccessor _httpAccessor;            
+
+        public UserIdService(IHttpContextAccessor httpAccessor)         
+        {                                                               
+            _httpAccessor = httpAccessor;                               
+        }                                                               
+
+        public Guid GetUserId()
+        {
+            var httpContext = _httpAccessor.HttpContext;                
+            if (httpContext == null)                                    
+                return Guid.Empty;                                      
+
+            var cookie = new BasketCookie(httpContext.Request.Cookies); 
+            if (!cookie.Exists())                                       
+                return Guid.Empty;                                      
+
+            var service = new CheckoutCookieService(cookie.GetValue()); 
+            return service.UserId;                                      
+        }
+    }
+}
