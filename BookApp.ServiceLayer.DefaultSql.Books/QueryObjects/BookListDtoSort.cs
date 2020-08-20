3d6dfc68d484b3dd016b20aeac299232bdc020f4
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-08-20 09:36:18 +0100

    Minor tidy up

--- BookApp.ServiceLayer.DefaultSql.Books/QueryObjects/BookListDtoSort.cs
+++ BookApp.ServiceLayer.DefaultSql.Books/QueryObjects/BookListDtoSort.cs
@@ -14,42 +14,34 @@ namespace BookApp.ServiceLayer.DefaultSql.Books.QueryObjects
         [Display(Name = "Votes ↑")] ByVotes,
         [Display(Name = "Publication Date ↑")] ByPublicationDate,
         [Display(Name = "Price ↓")] ByPriceLowestFirst,
-        [Display(Name = "Price ↑")] ByPriceHigestFirst
+        [Display(Name = "Price ↑")] ByPriceHighestFirst
     }
 
     public static class BookListDtoSort
     {
         public static IQueryable<BookListDto> OrderBooksBy
-        (this IQueryable<BookListDto> books,
-            OrderByOptions orderByOptions)
+            (this IQueryable<BookListDto> books, OrderByOptions orderByOptions)
         {
             switch (orderByOptions)
             {
-                case OrderByOptions.SimpleOrder: //#A
-                    return books.OrderByDescending( //#A
-                        x => x.BookId); //#A
-                case OrderByOptions.ByVotes: //#B
-                    return books.OrderByDescending(x => //#B
-                        x.ReviewsAverageVotes); //#B
-                case OrderByOptions.ByPublicationDate: //#C
-                    return books.OrderByDescending( //#C
-                        x => x.PublishedOn); //#C
-                case OrderByOptions.ByPriceLowestFirst: //#D
-                    return books.OrderBy(x => x.ActualPrice); //#D
-                case OrderByOptions.ByPriceHigestFirst: //#D
-                    return books.OrderByDescending( //#D
-                        x => x.ActualPrice); //#D
+                case OrderByOptions.SimpleOrder: 
+                    return books.OrderByDescending( 
+                        x => x.BookId); 
+                case OrderByOptions.ByVotes: 
+                    return books.OrderByDescending(x => 
+                        x.ReviewsAverageVotes); 
+                case OrderByOptions.ByPublicationDate: 
+                    return books.OrderByDescending( 
+                        x => x.PublishedOn); 
+                case OrderByOptions.ByPriceLowestFirst: 
+                    return books.OrderBy(x => x.ActualPrice); 
+                case OrderByOptions.ByPriceHighestFirst: 
+                    return books.OrderByDescending( 
+                        x => x.ActualPrice); 
                 default:
                     throw new ArgumentOutOfRangeException(
                         nameof(orderByOptions), orderByOptions, null);
             }
         }
-
-        /************************************************************
-        #A Because of paging we always need to sort. I default to showing latest entries first
-        #B This orders the book by votes. Books without any votes (null return) go at the bottom
-        #C Order by publication date - latest books at the top
-        #D Order by actual price, which takes into account any promotional price - both lowest first and highest first
-         * ********************************************************/
     }
 }
