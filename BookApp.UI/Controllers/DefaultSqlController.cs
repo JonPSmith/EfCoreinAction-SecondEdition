21201254b408aa8d99866af1d87f732477dd657e
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-08-20 10:55:17 +0100

    Added Default SQL controller

---
+++ BookApp.UI/Controllers/DefaultSqlController.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) 2020 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookApp.Infrastructure.LoggingServices;
+using BookApp.Persistence.EfCoreSql.Books;
+using BookApp.ServiceLayer.DefaultSql.Books;
+using BookApp.ServiceLayer.DefaultSql.Books.Dtos;
+using BookApp.ServiceLayer.DefaultSql.Books.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApp.UI.Controllers
+{
+    public class DefaultSqlController : BaseTraceController
+    {
+        private readonly BookDbContext _context;
+
+        public DefaultSqlController(BookDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(SortFilterPageOptions options)
+        {
+            var listService = new ListBooksService(_context);
+
+            var bookList = await (await listService.SortFilterPageAsync(options))
+                .ToListAsync();
+
+            SetupTraceInfo();
+
+            return View(new BookListCombinedDto(options, bookList));
+        }
+
+        /// <summary>
+        /// This provides the filter search dropdown content
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult GetFilterSearchContent
+            (SortFilterPageOptions options)
+        {
+            var service = new
+                BookFilterDropdownService(_context);
+
+            var traceIdent = HttpContext.TraceIdentifier; //REMOVE THIS FOR BOOK as it could be confusing
+
+            return Json(
+                new TraceIndentGeneric<IEnumerable<DropdownTuple>>(
+                    traceIdent,
+                    service.GetFilterDropDownValues(
+                        options.FilterBy)));
+        }
+    }
+}
--- BookApp.UI/Controllers/HomeController.cs
+++ BookApp.UI/Controllers/HomeController.cs
@@ -19,44 +19,11 @@ namespace BookApp.UI.Controllers
 {
     public class HomeController : BaseTraceController
     {
-        private readonly BookDbContext _context;
 
-        public HomeController(BookDbContext context)
+
+        public IActionResult Index()
         {
-            _context = context;
-        }
-
-        public async Task<IActionResult> Index(SortFilterPageOptions options)
-        {
-            var listService = new ListBooksService(_context);
-
-            var bookList = await (await listService.SortFilterPageAsync(options))
-                .ToListAsync();
-
-            SetupTraceInfo();
-
-            return View(new BookListCombinedDto(options, bookList));
-        }
-
-        /// <summary>
-        /// This provides the filter search dropdown content
-        /// </summary>
-        /// <param name="options"></param>
-        /// <returns></returns>
-        [HttpGet]
-        public JsonResult GetFilterSearchContent 
-            (SortFilterPageOptions options) 
-        {
-            var service = new 
-                BookFilterDropdownService(_context); 
-
-            var traceIdent = HttpContext.TraceIdentifier; //REMOVE THIS FOR BOOK as it could be confusing
-
-            return Json( 
-                new TraceIndentGeneric<IEnumerable<DropdownTuple>>(
-                    traceIdent,
-                    service.GetFilterDropDownValues( 
-                        options.FilterBy))); 
+            return View();
         }
 
         public IActionResult Privacy()
