1254a9913a83e42f9071e404d1b284697504e817
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-08-28 15:52:59 +0100

    Added comments for book

--- Test/Chapter12Listings/EventRunnerCode/EventHandlerRunner.cs
+++ Test/Chapter12Listings/EventRunnerCode/EventHandlerRunner.cs
@@ -2,28 +2,35 @@
 // Licensed under MIT license. See License.txt in the project root for license information.
 
 using Test.Chapter12Listings.EventInterfacesEtc;
-using Test.Chapter12Listings.Events;
 
 namespace Test.Chapter12Listings.EventRunnerCode
 {
-    internal abstract class EventHandlerRunner
+    internal abstract class EventHandlerRunner               //#A        
     {
-        public abstract void HandleEvent(IDomainEvent domainEvent);
+        public abstract void HandleEvent                     //#A
+            (IDomainEvent domainEvent);                      //#A
     }
 
-    internal class EventHandlerRunner<T> : EventHandlerRunner
+    internal class EventHandlerRunner<T> : EventHandlerRunner //#B
         where T : IDomainEvent
     {
-        private readonly IEventHandler<T> _handler;
+        private readonly IEventHandler<T> _handler;           //#C
 
-        public EventHandlerRunner(IEventHandler<T> handler)
-        {
-            _handler = handler;
-        }
+        public EventHandlerRunner(IEventHandler<T> handler)   //#C
+        {                                                     //#C
+            _handler = handler;                               //#C
+        }                                                     //#C
 
-        public override void HandleEvent(IDomainEvent domainEvent)
-        {
-            _handler.HandleEvent((T)domainEvent);
-        }
+        public override void HandleEvent                      //#D
+            (IDomainEvent domainEvent)                        //#D
+        {                                                     //#D
+            _handler.HandleEvent((T)domainEvent);             //#D
+        }                                                     //#D
     }
+    /****************************************************
+    #A By defining a non-generic class+method you can cast the created generic instance to an non-generic version
+    #B This uses the EventHandlerRunner<T> to define the type of the EventHandlerRunner
+    #C The EventHandlerRunner class is created with an instance of the event handler to run
+    #D Now to the method the abstract class defined.
+     ***********************************************/
 }
--- Test/Chapter12Listings/EventRunnerCode/EventRunner.cs
+++ Test/Chapter12Listings/EventRunnerCode/EventRunner.cs
@@ -5,37 +5,54 @@ using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Test.Chapter12Listings.EventInterfacesEtc;
-using Test.Chapter12Listings.Events;
 
 namespace Test.Chapter12Listings.EventRunnerCode
 {
-    public class EventRunner : IEventRunner
+    public class EventRunner : IEventRunner                          //#A
     {
-        private readonly IServiceProvider _serviceProvider;
-
-        public EventRunner(IServiceProvider serviceProvider)
-        {
-            _serviceProvider = serviceProvider;
-        }
+        private readonly IServiceProvider _serviceProvider;          //#B
+                                                                     //#B
+        public EventRunner(IServiceProvider serviceProvider)         //#B
+        {                                                            //#B
+            _serviceProvider = serviceProvider;                      //#B
+        }                                                            //#B
 
         public void RunEvents(DbContext context)
         {
-            var allEvents  = context.ChangeTracker.Entries<IEntityEvents>()
-                .SelectMany(x => x.Entity.GetEventsThenClear());
+            var allEvents  = context.                                //#C
+                ChangeTracker.Entries<IEntityEvents>()               //#C
+                .SelectMany(x => x.Entity.GetEventsThenClear());     //#C
 
-            foreach (var domainEvent in allEvents)
+            foreach (var domainEvent in allEvents)                   //#D
             {
-                var domainEventType = domainEvent.GetType();
-                var handleType = typeof(IEventHandler<>).MakeGenericType(domainEventType);
-                var eventHandler = _serviceProvider.GetService(handleType);
-                if (eventHandler == null)
-                    throw new InvalidOperationException(
-                        $"Could not find an event handler for the event {handleType.Name}");
+                var domainEventType = domainEvent.GetType();         //#E
+                var eventHandleType = typeof(IEventHandler<>)        //#E
+                    .MakeGenericType(domainEventType);               //#E
 
-                var handlerRunner =
-                    ((EventHandlerRunner) Activator.CreateInstance(typeof(EventHandlerRunner<>).MakeGenericType(domainEventType), eventHandler));
-                handlerRunner.HandleEvent(domainEvent);
+                var eventHandler =                                   //#F
+                    _serviceProvider.GetService(eventHandleType);    //#F
+                if (eventHandler == null)                            //#F
+                    throw new InvalidOperationException(             //#F
+                        $"Could not find an event handler for the event {eventHandleType.Name}");
+
+                var handlerRunnerType = typeof(EventHandlerRunner<>) //#G
+                    .MakeGenericType(domainEventType);               //#G
+                var handlerRunner = ((EventHandlerRunner)            //#G
+                    Activator.CreateInstance(                        //#G
+                        handlerRunnerType, eventHandler));           //#G
+
+                handlerRunner.HandleEvent(domainEvent);              //#H
             }
         }
     }
+    /*********************************************************************
+    #A The EventRunner needs an interface to that you can register it with the DI
+    #B The event runner needs the ServiceProvider to get an instance of the event handlers
+    #C This reads in all the events. It also clears the entity events to stops duplicate events
+    #D This loops through each event found
+    #E This gets the interface type of the matching event handler
+    #F This uses the DI provider to create an instance of the event handler - error if not found
+    #G This creates the EventHandlerRunner that you need to run the event handler
+    #H Then you use the EventHandlerRunner to run the event handler
+     *********************************************************************/
 }
