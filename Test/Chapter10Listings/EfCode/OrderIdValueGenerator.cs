38d9fabc3e432704611deb2999eb4b53fe24d17b
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-09-07 14:13:41 +0100

    Updated to work across paralle apps

--- Test/Chapter10Listings/EfCode/OrderIdValueGenerator.cs
+++ Test/Chapter10Listings/EfCode/OrderIdValueGenerator.cs
@@ -12,24 +12,29 @@ namespace Test.Chapter10Listings.EfCode
         : ValueGenerator<string> // #A
     {
         public override bool 
-            GeneratesTemporaryValues => false; //#B
+            GeneratesTemporaryValues => false;            //#B
 
-        public override string Next //#C
+        public override string Next                       //#C
             (EntityEntry entry)
         {
-            var name = entry. //#D
-                Property(nameof(DefaultTest.Name)) //#E
+            var name = entry.                             //#D
+                Property(nameof(DefaultTest.Name))        //#E
                     .CurrentValue;
-            var uniqueNum = DateTime.UtcNow.Ticks;
-            return $"{name}-{uniqueNum}"; //#F
+            var ticks = DateTime.UtcNow.ToString("s");    //#F
+            var guidString = Guid.NewGuid().ToString();   //#G
+            var orderId = $"{name}-{ticks}-{guidString}"; //#H
+            return orderId;                               //#I
         }
     }
     /**********************************************************
-    #A My value generator needs to inherit from EF Core's ValueGenerator<T>
+    #A The value generator needs to inherit from EF Core's ValueGenerator<T>
     #B Set this to false if you want your value to be written to the database
     #C This method is called when you Add the entity to the DbContext
     #D The parameter gives you access to the entity that the value generator is creating a value for. You can access its properties etc.
     #E I select the property called "Name" and get its current value
-    #F You need to return a value of the Type you have defined at T in the inherted ValueGenerator<T> 
+    #F This provides the date in a sortable format
+    #G This provides a unique string 
+    #H The orderId combines these three parts to create a unique orderId with useful info in it 
+    #I The method must return a value of the Type you have defined at T in the inherited ValueGenerator<T> 
         * ********************************************************/
 }
