4d7cff048ca077bd00a99daa1bb7e7d761cca0af
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-10-13 15:59:37 +0100

    Code on SaveChanges events and interception

---
+++ Test/UnitTests/TestDataLayer/Ch11_SaveChangesEvents.cs
@@ -0,0 +1,205 @@
+﻿// Copyright (c) 2020 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Test.Chapter11Listings.EfClasses;
+using Test.Chapter11Listings.EfCode;
+using Test.Chapter11Listings.Interfaces;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Test.UnitTests.TestDataLayer
+{
+    public class Ch11_SaveChangesEvents
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Ch11_SaveChangesEvents(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+
+        [Fact]
+        public void TestSavingChangesOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter11DbContext>();
+            using (var context = new Chapter11DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                context.SavingChanges += delegate
+                (object dbContext,
+                    SavingChangesEventArgs args)
+                {
+                    //VERIFY
+                    context.ShouldBeSameAs(dbContext);
+                    context.ChangeTracker.Entries().Single().State.ShouldEqual(EntityState.Added);
+                };
+
+                //ATTEMPT
+                var entity = new MyEntity {MyString = "Test"};
+                context.Add(entity);   
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public void TestSavedChangesOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter11DbContext>();
+            using (var context = new Chapter11DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                context.SavedChanges += delegate
+                (object dbContext,
+                    SavedChangesEventArgs args)
+                {
+                    //VERIFY
+                    context.ShouldBeSameAs(dbContext);
+                    context.ChangeTracker.Entries().Single().State.ShouldEqual(EntityState.Unchanged);
+                };
+
+                //ATTEMPT
+                var entity = new MyEntity { MyString = "Test" };
+                context.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async Task TestSavingChangesAsyncOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter11DbContext>();
+            using (var context = new Chapter11DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                context.SavingChanges += delegate
+                (object dbContext,
+                    SavingChangesEventArgs args)
+                {
+                    //VERIFY
+                    context.ShouldBeSameAs(dbContext);
+                    context.ChangeTracker.Entries().Single().State.ShouldEqual(EntityState.Added);
+                };
+
+                //ATTEMPT
+                var entity = new MyEntity { MyString = "Test" };
+                context.Add(entity);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        [Fact]
+        public void TestSaveChangesFailedOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter11DbContext>();
+            using (var context = new Chapter11DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                context.SaveChangesFailed += delegate
+                (object dbContext,
+                    SaveChangesFailedEventArgs args)
+                {
+
+                };
+
+                //ATTEMPT
+                var entity = new MyEntity { MyString = "Test" };
+                context.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public void TestSavingChangesAlterOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter11DbContext>();
+            using (var context = new Chapter11DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                var entity = new EntityAddUpdate { Name = "Test1" };
+                context.Add(entity);
+                context.SaveChanges();
+            }
+            using (var context = new Chapter11DbContext(options))
+            {
+                context.SavingChanges += delegate
+                    (object dbContext, 
+                    SavingChangesEventArgs args)
+                {
+                    foreach (var entity in ((DbContext)dbContext)
+                        .ChangeTracker.Entries()
+                        .Where(e =>
+                            e.State == EntityState.Added ||
+                            e.State == EntityState.Modified))
+                    {
+                        var tracked = entity.Entity as ICreatedUpdated;
+                        tracked?.LogChange(entity);
+                    }
+
+                };
+
+                //ATTEMPT
+                var updateEntity = context.LoggedEntities.Single();
+                updateEntity.Name = "Test2";
+                var addEntity = new EntityAddUpdate { Name = "Test3" };
+                context.Add(addEntity);
+                context.SaveChanges();
+
+                //VERIFY
+                updateEntity.WhenCreatedUtc.ShouldNotEqual(updateEntity.LastUpdatedUtc);
+                addEntity.WhenCreatedUtc.ShouldEqual(addEntity.LastUpdatedUtc);
+            }
+        }
+
+
+        [Fact]
+        public void TestSavingChangesEventsLinkedToContextOk()
+        {
+            //SETUP
+            var logs = new List<EntityEntry>();
+            var options = SqliteInMemory.CreateOptions<Chapter11DbContext>();
+            using (var context = new Chapter11DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                context.SavingChanges += (dbContext, args) =>
+                {
+                    context.ChangeTracker.Entries().ToList().ForEach(x => logs.Add(x));
+                };
+
+                //ATTEMPT
+                var entity = new MyEntity {MyString = "Test1"};
+                context.Add(entity);
+                context.SaveChanges();
+            }
+            using (var context = new Chapter11DbContext(options))
+            {
+                var entity = new MyEntity { MyString = "Test2" };
+                context.Add(entity);
+                context.SaveChanges();
+
+                //VERIFY
+                logs.Count.ShouldEqual(1);
+                ((MyEntity)logs.Single().Entity).MyString.ShouldEqual("Test1");
+            }
+        }
+
+    }
+}
---
+++ Test/UnitTests/TestDataLayer/Ch11_SaveChangesInterceptor.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) 2020 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DataLayer.EfClasses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Test.Chapter11Listings.EfClasses;
+using Test.Chapter11Listings.EfCode;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Test.UnitTests.TestDataLayer
+{
+    public class Ch11_SaveChangesInterceptor
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Ch11_SaveChangesInterceptor(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public class MySaveChangesInterceptor : SaveChangesInterceptor
+        {
+            private readonly bool _stopSaveChanges;
+
+            public MySaveChangesInterceptor(bool stopSaveChanges)
+            {
+                _stopSaveChanges = stopSaveChanges;
+            }
+
+            public override InterceptionResult<int> SavingChanges(
+                DbContextEventData eventData,
+                InterceptionResult<int> result)
+            {
+                eventData.Context.ChangeTracker.Entries().Single().State.ShouldEqual(EntityState.Added);
+
+                return _stopSaveChanges ? InterceptionResult<int>.SuppressWithResult(-1) : result;
+            }
+
+            public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+                DbContextEventData eventData,
+                InterceptionResult<int> result,
+                CancellationToken cancellationToken = new CancellationToken())
+            {
+                Console.WriteLine($"Saving changes asynchronously for {eventData.Context.Database.GetConnectionString()}");
+
+                return _stopSaveChanges 
+                    ? new ValueTask<InterceptionResult<int>>(InterceptionResult<int>.SuppressWithResult(-1))
+                    : new ValueTask<InterceptionResult<int>>(result);
+            }
+        }
+
+        [Theory]
+        [InlineData(false, 1)]
+        [InlineData(true, -1)]
+        public void TestSaveChangesInterceptorOk(bool stopSaveChanges, int expectedResult)
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter11DbContext>( builder => 
+                builder.AddInterceptors(new MySaveChangesInterceptor(stopSaveChanges)));
+            using (var context = new Chapter11DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                //ATTEMPT
+                var entity = new MyEntity {MyString = "Test"};
+                context.Add(entity);   
+                var result = context.SaveChanges();
+
+                //VERIFY
+                result.ShouldEqual(expectedResult);
+                context.MyEntities.Count().ShouldEqual(stopSaveChanges ? 0 : 1);
+            }
+        }
+        
+    }
+}
