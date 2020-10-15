ae546d6e705ce1c80200518f9607c0e200315ca3
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-10-15 15:27:36 +0100

    Added comments

--- BookApp.Domain.Books/Book.cs
+++ BookApp.Domain.Books/Book.cs
@@ -15,19 +15,80 @@ namespace BookApp.Domain.Books
     public class Book : EntityEventsBase, ISoftDelete
     {
         public const int PromotionalTextLength = 200;
-        private HashSet<BookAuthor> _authorsLink;
 
         //-----------------------------------------------
-        //relationships
+        //relationships backing fields
 
         //Use uninitialized backing fields - this means we can detect if the collection was loaded
         private HashSet<Review> _reviews;
+        private HashSet<BookAuthor> _authorsLink;
         private HashSet<Tag> _tags;
 
         //-----------------------------------------------
-        //ctors
+        //ctors/static factory
 
-        private Book() { } //Needed by EF Core
+        private Book() { }  //#A //Needed by EF Core
+
+        public static IStatusGeneric<Book> CreateBook(      //#B
+            string title, DateTime publishedOn,              //#C
+            bool estimatedDate,                              //#C
+            string publisher, decimal price, string imageUrl,//#C
+            ICollection<Author> authors,                     //#C
+            ICollection<Tag> tags = null)                    //#D
+        {
+            var status = new StatusGenericHandler<Book>();  //#E
+            if (string.IsNullOrWhiteSpace(title))         //#F
+                status.AddError(                          //#F
+                    "The book title cannot be empty.");   //#F
+
+            var book = new Book                         //#G
+            {                                           //#G
+                Title = title,                          //#G
+                PublishedOn = publishedOn,              //#G
+                EstimatedDate = estimatedDate,          //#G
+                Publisher = publisher,
+                OrgPrice = price,
+                ActualPrice = price,
+                ImageUrl = imageUrl,
+                //We need to initialise the AuthorsOrdered string when the entry is created
+                //NOTE: We must NOT initialise the ReviewsCount and the ReviewsAverageVotes as they default to zero
+                AuthorsOrdered = string.Join(", ", authors.Select(x => x.Name)),
+
+                _tags = tags != null           //#H
+                    ? new HashSet<Tag>(tags)   //#H
+                    : new HashSet<Tag>(),      //#H
+                _reviews = new HashSet<Review>()       //We add an empty list on create. I allows reviews to be added when building test data
+            };
+            if (authors == null)                                   //#I
+                throw new ArgumentNullException(nameof(authors));  //#I
+
+            byte order = 0;                               //#J
+            book._authorsLink = new HashSet<BookAuthor>(  //#J
+                authors.Select(a =>                       //#J
+                    new BookAuthor(book, a, order++)));   //#J
+            if (!book._authorsLink.Any())                             //#K
+                status.AddError(                                      //#K
+                    "You must have at least one Author for a book."); //#K
+
+            return status.SetResult(book); //#L
+        }
+        /***********************************************************************************
+        #A Creating a private constructor means people can't create the entity via a constructor
+        #B The static CreateBook method returns a status with a valid Book (if there are no errors)
+        #C These all the parameters that are needed to create a valid book
+        #D The Tags are optional
+        #E This creates a status that can return a result - in this case a Book
+        #F This adds an error. Note it doesn't return immediately so that other errors can be added
+        #G Now you set the properties 
+        #H This sets up the Tags collection via the backing field
+        #I The authors parameter being null is considered a software error and throws an exception
+        #J This creates the BookAuthor class in the order that the Authors have been provided
+        #K If there are no Authors we add an error
+        #L This sets the status's Result to the new Book instance. But if there are errors it will be null
+         ************************************************************************/
+
+        //---------------------------------------
+        //scalar properties
 
         public int BookId { get; private set; }
 
@@ -47,13 +108,13 @@ namespace BookApp.Domain.Books
         [MaxLength(200)]
         public string ImageUrl { get; private set; }
 
-
         /// <summary>
         /// This contains the url to get to the Manning version of the book
         /// </summary>
         public string ManningBookUrl { get; private set; }
 
         //---------------------------------------------
+        //Soft delete
 
         public bool SoftDeleted { get; private set; }
 
@@ -88,42 +149,6 @@ namespace BookApp.Domain.Books
             ReviewsAverageVotes = reviewsAverageVotes;
         }
         //----------------------------------------------
-
-        public static IStatusGeneric<Book> CreateBook(string title, DateTime publishedOn,
-            bool estimatedDate,
-            string publisher, decimal price, string imageUrl,
-            ICollection<Author> authors, ICollection<Tag> tags = null)
-        {
-            var status = new StatusGenericHandler<Book>();
-            if (string.IsNullOrWhiteSpace(title))
-                status.AddError("The book title cannot be empty.");
-
-            var book = new Book
-            {
-                Title = title,
-                PublishedOn = publishedOn,
-                EstimatedDate = estimatedDate,
-                Publisher = publisher,
-                OrgPrice = price,
-                ActualPrice = price,
-                ImageUrl = imageUrl,
-                //We need to initialise the AuthorsOrdered string when the entry is created
-                //NOTE: We must NOT initialise the ReviewsCount and the ReviewsAverageVotes as they default to zero
-                AuthorsOrdered = string.Join(", ", authors.Select(x => x.Name)),
-
-                _tags = tags != null ? new HashSet<Tag>(tags) : new HashSet<Tag>(),
-                _reviews = new HashSet<Review>()       //We add an empty list on create. I allows reviews to be added when building test data
-            };
-            if (authors == null)
-                throw new ArgumentNullException(nameof(authors));
-            byte order = 0;
-            book._authorsLink = new HashSet<BookAuthor>(authors.Select(a => new BookAuthor(book, a, order++)));
-            if (!book._authorsLink.Any())
-                status.AddError("You must have at least one Author for a book.");
-            
-
-            return status.SetResult(book);
-        }
 
         public override string ToString()
         {
@@ -168,50 +193,75 @@ namespace BookApp.Domain.Books
         }
 
         //This works with the GenericServices' IncludeThen Attribute to pre-load the Reviews collection
-        public void AddReview(int numStars, string comment, string voterName)
+        public void AddReview(int numStars,   //#A
+            string comment, string voterName) //#A
         {
-            if (_reviews == null)
-                throw new InvalidOperationException("The Reviews collection must be loaded before calling this method");
-            _reviews.Add(new Review(numStars, comment, voterName));
+            if (_reviews == null)                      //#B
+                throw new InvalidOperationException(   //#B
+                    "The Reviews collection must be loaded before calling this method");
+            _reviews.Add(new Review(           //#C
+                numStars, comment, voterName));    //#C
 
             AddEvent(new BookReviewAddedEvent(numStars, this, UpdateReviewCachedValues));
         }
 
         //This works with the GenericServices' IncludeThen Attribute to pre-load the Reviews collection
-        public void RemoveReview(int reviewId)
+        public void RemoveReview(int reviewId) //#D
         {
-            if (_reviews == null)
-                throw new InvalidOperationException("The Reviews collection must be loaded before calling this method");
-            var localReview = _reviews.SingleOrDefault(x => x.ReviewId == reviewId);
-            if (localReview == null)
-                throw new InvalidOperationException("The review with that key was not found in the book's Reviews.");
-            _reviews.Remove(localReview);
+            if (_reviews == null)                    //#B
+                throw new InvalidOperationException( //#B
+                    "The Reviews collection must be loaded before calling this method");
+            var localReview = _reviews.SingleOrDefault(  //#E
+                x => x.ReviewId == reviewId);            //#E
+            if (localReview == null)                  //#F
+                throw new InvalidOperationException(  //#F
+                    "The review with that key was not found in the book's Reviews.");
+            _reviews.Remove(localReview);             //#G
             
             AddEvent(new BookReviewRemovedEvent(localReview, this, UpdateReviewCachedValues));
         }
+        /*****************************************************************
+        #A This adds a new review with the given parameters
+        #B This code relies on the _reviews being loaded so it throws an exception if it isn't
+        #C This creates a new Review using its internal constructor
+        #D This removes a review using its primary key
+        #E This finds the specific Review to remove
+        #F Not finding the review is considered a software error, so it throws an exception
+        #G The found review is removed
+         ******************************************************************/
 
-        public IStatusGeneric AddPromotion(decimal actualPrice, string promotionalText)                  
+        public IStatusGeneric AddPromotion(               //#A
+            decimal actualPrice, string promotionalText)  //#B               
         {
-            var status = new StatusGenericHandler();
-            if (string.IsNullOrWhiteSpace(promotionalText))
+            var status = new StatusGenericHandler();      //#C
+            if (string.IsNullOrWhiteSpace(promotionalText)) //#D
             {
-                status.AddError("You must provide some text to go with the promotion.", nameof(PromotionalText));
-                return status;
+                return status.AddError(                        //#E
+                    "You must provide text to go with the promotion.", //#F
+                    nameof(PromotionalText));     //#F
             }
 
-            ActualPrice = actualPrice;  
-            PromotionalText = promotionalText;
+            ActualPrice = actualPrice;         //#G
+            PromotionalText = promotionalText; //#G
 
-            status.Message = $"The book's new price is ${actualPrice:F}.";
-
-            return status; 
+            return status; //#H
         }
 
-        public void RemovePromotion() 
+        public void RemovePromotion() //#H
         {
-            ActualPrice = OrgPrice; 
-            PromotionalText = null; 
+            ActualPrice = OrgPrice; //#I
+            PromotionalText = null; //#I
         }
+        /******************************************************************
+        #A The AddPromotion return a status: successful if no error and returns errors 
+        #B The parameters came from the input
+        #C This creates a status which is successful unless errors are added to it
+        #D You ensure the promotionalText has some text in it
+        #E The AddError method adds an error, and it returned immediately
+        #F The error contains a user-friendly message and the name of the property that has the error
+        #G If no errors, then the ActualPrice and PromotionalText are updated
+        #H The status, which is successful, is returned
+         *******************************************************************/
     }
 
 }
