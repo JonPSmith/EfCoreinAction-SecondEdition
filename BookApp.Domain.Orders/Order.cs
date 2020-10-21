d4f1e44bd6f92c2e656cea58f41dbfc0b653fc50
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-10-21 10:33:55 +0100

    Added comments

--- BookApp.Domain.Orders/Order.cs
+++ BookApp.Domain.Orders/Order.cs
@@ -24,24 +24,37 @@ namespace BookApp.Domain.Orders
 
         public IReadOnlyCollection<LineItem> LineItems => _lineItems?.ToList();
 
-        public static IStatusGeneric<Order> CreateOrder(Guid userId,
-            IEnumerable<OrderBookDto> bookOrders)
+        public static IStatusGeneric<Order> CreateOrder  //#A
+            (Guid userId,  //#B
+            IEnumerable<OrderBookDto> bookOrders) //#C
         {
-            var status = new StatusGenericHandler<Order>();
-            var order = new Order
-            {
-                UserId = userId,
-                DateOrderedUtc = DateTime.UtcNow
-            };
+            var status = new StatusGenericHandler<Order>(); //#D
+            var order = new Order                 //#E
+            {                                     //#E
+                UserId = userId,                  //#E
+                DateOrderedUtc = DateTime.UtcNow  //#E
+            };                                    //#E
 
             byte lineNum = 1;
-            order._lineItems = new HashSet<LineItem>(bookOrders
-                .Select(x => new LineItem( x, lineNum++)));
-            if (!order._lineItems.Any())
-                status.AddError("No items in your basket.");
+            order._lineItems = new HashSet<LineItem>(        //#F
+                bookOrders                                   //#F
+                .Select(x => new LineItem( x, lineNum++)));  //#F
 
-            return status.SetResult(order); //don't worry, the Result will return default(T) if there are errors
+            if (!order._lineItems.Any())                     //#G
+                status.AddError("No items in your basket."); //#G
+
+            return status.SetResult(order); //#H
         }
+        /***************************************************************
+        #A This static factory will create the Order with lineItems
+        #B The Order uses the UserId to only show orders to the person who created it
+        #C The OrderBookDto live the Order domain and carries the info the Order needs
+        #D Create a status to return with an optional result of Order
+        #E This sets the standard properties in an order
+        #F This creates each of the LineItems in the same order the user added them
+        #G This is a double-check that the Order is valid.
+        #H This returns the status with the Order. If there are errors the status sets the result to null
+         ***************************************************************/
 
     }
 }
--- BookApp.Infrastructure.Orders.BizLogic/Orders/Concrete/PlaceOrderBizLogic.cs
+++ BookApp.Infrastructure.Orders.BizLogic/Orders/Concrete/PlaceOrderBizLogic.cs
@@ -26,34 +26,52 @@ namespace BookApp.Infrastructure.Orders.BizLogic.Orders.Concrete
         /// </summary>
         /// <param name="dto">T and Cs, UserId and line items</param>
         /// <returns>returns an Order. Will be null if there are errors</returns>
-        public async Task<IStatusGeneric<Order>> CreateOrderAndSaveAsync(PlaceOrderInDto dto) 
+        public async Task<IStatusGeneric<Order>>   //#A
+            CreateOrderAndSaveAsync(PlaceOrderInDto dto) //#B
         {
-            var status = new StatusGenericHandler<Order>();
+            var status = new StatusGenericHandler<Order>(); //#C
 
-            if (!dto.AcceptTAndCs)                    
+            if (!dto.AcceptTAndCs)                    //#D
             {
                 return status.AddError("You must accept the T&Cs to place an order.");
             }                                         
-            if (!dto.LineItems.Any())                 
+            if (!dto.LineItems.Any())                 //#D
             {
                 return status.AddError("No items in your basket.");
             }                                         
 
-            var booksDict = await _dbAccess.FindBooksByIdsAsync    
-                     (dto.LineItems.Select(x => x.BookId));
-            var linesStatus = FormLineItemsWithErrorChecking(dto.LineItems, booksDict);
-            if (status.CombineStatuses(linesStatus).HasErrors)
-                return status;
-
-            var orderStatus = Order.CreateOrder(dto.UserId, linesStatus.Result);
-
-            if (status.CombineStatuses(orderStatus).HasErrors)
-                return status;
-                
-            await _dbAccess.AddAndSave(orderStatus.Result);
+            var booksDict = await _dbAccess                 //#E
+                .FindBooksByIdsAsync                        //#E
+                     (dto.LineItems.Select(x => x.BookId)); //#E
             
-            return status.SetResult(orderStatus.Result);
+            var linesStatus = FormLineItemsWithErrorChecking //#F
+                (dto.LineItems, booksDict);                  //#F
+            if (status.CombineStatuses(linesStatus).HasErrors)//#G
+                return status;                                //#G
+
+            var orderStatus = Order.CreateOrder(  //#H
+                dto.UserId, linesStatus.Result);  //#H
+
+            if (status.CombineStatuses(orderStatus).HasErrors)//#I
+                return status;                                //#I
+                
+            await _dbAccess.AddAndSave(orderStatus.Result); //#J
+            
+            return status.SetResult(orderStatus.Result); //#K
         }
+        /*******************************************************************
+        #A This method returns a status with the created Order, which is null if there are no errors
+        #B The PlaceOrderInDto contains a TandC bool and a collection of BookIds and number of books 
+        #C This status is used to gather and errors and, if no errors, return an Order
+        #D These validate the user's input
+        #E The _dbAccess contains the code to find each book - see listing 4.3
+        #F This method creates list of bookId and number of books - see end of listing 4.2
+        #G If any errors were found while checking each order line, then it returns the error status
+        #H This calls the Order static factory. It is the Order's job to form the Order with LineItems
+        #I Again, any errors will abort the Order and the errors returned
+        #J The _dbAccess contains the code add the Order and call SaveChangesAsync
+        #K Finally it returns a successful status with the created Order entity
+         ****************************************************************/
 
         private IStatusGeneric<List<OrderBookDto>>  FormLineItemsWithErrorChecking
             (IEnumerable<OrderLineItem> lineItems,            
