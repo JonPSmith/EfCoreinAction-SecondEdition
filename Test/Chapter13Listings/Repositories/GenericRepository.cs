1d07b232b93e95d1966a187fd59b05ee6ff1dfe8
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-10-22 09:11:52 +0100

    Updated comments for book

--- Test/Chapter13Listings/Repositories/GenericRepository.cs
+++ Test/Chapter13Listings/Repositories/GenericRepository.cs
@@ -26,24 +26,27 @@ namespace Test.Chapter13Listings.Repositories
 
         public async Task<TEntity> FindEntityAsync(int id)  //#D
         {
-            var entity = await Context.FindAsync<TEntity>(id); //#D
-            if (entity == null)                               //#E
-                throw new Exception("Could not find entity"); //#E
-            return entity; //#F
+            var entity = await Context.FindAsync<TEntity>(id); //#E
+
+            if (entity == null)                               //#F
+                throw new Exception("Could not find entity"); //#F 
+            
+            return entity; //#G
         }
 
-        public Task PersistDataAsync()        //#G
-        {                                     //#G
-            return Context.SaveChangesAsync();//#G
-        }                                     //#G
+        public Task PersistDataAsync()        //#H
+        {                                     //#H
+            return Context.SaveChangesAsync();//#H
+        }                                     //#H
     }
     /**************************************************************
     #A The generic repository will work with any entity class
     #B The repository needs the DbContext of the database
     #C This returns an IQueryable query of the entity type
-    #D This finds an entity via its single, integer primary key
-    #E This a rudimentary check that the entity was found
-    #F The found entity is returned
-    #G This calls SaveChanges to update the database
+    #D This method finds and returns a entity with a integer primary key
+    #E This finds an entity via its single, integer primary key
+    #F This a rudimentary check that the entity was found
+    #G The found entity is returned
+    #H This calls SaveChanges to update the database
      ***************************************************************/
 }
