225d2c563f6703e55cef7c5467a9c69cfe6772cf
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-11-22 09:10:15 +0000

    Added test to check log output from Cosmos DB database provider

--- Test/TestHelpers/CosmosSetupHelpers.cs
+++ Test/TestHelpers/CosmosSetupHelpers.cs
@@ -12,7 +12,7 @@ namespace Test.TestHelpers
 {
     public static class CosmosSetupHelpers
     {
-        private const string CosmosConnectionName = "CosmosConnectionString";
+        public const string CosmosConnectionName = "CosmosConnectionString";
 
         private const string CosmosEmulatorCon =
             "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
