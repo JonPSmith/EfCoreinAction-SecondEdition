cfb51e3a2ade851daa5d8d82f1ff03ad85a18af3
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-11-22 18:31:50 +0000

    Updated to latest GenericEventRunner - now won't regsiter even handler that is already registered

--- BookApp.Infrastructure.Book.CachedValues/EventHandlers/ReviewAddedHandler.cs
+++ BookApp.Infrastructure.Book.CachedValues/EventHandlers/ReviewAddedHandler.cs
@@ -9,7 +9,6 @@ using StatusGeneric;
 
 namespace BookApp.Infrastructure.Books.CachedValues.EventHandlers
 {
-    [DoNotAutoRegister]
     public class ReviewAddedHandler :                 
         IBeforeSaveEventHandler<BookReviewAddedEvent> //#A
     {
