e58bb6744aa69d1d889b8b44cdfb88c19d7140a5
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-11-26 14:36:02 +0000

    AsNoTracking test with HashSet

---
+++ Test/UnitTests/Chapter17Tests/TestAsNoTrackingHashset.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) 2020 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System.Linq;
+using BookApp.Persistence.EfCoreSql.Books;
+using Microsoft.EntityFrameworkCore;
+using Test.TestHelpers;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Test.UnitTests.Chapter17Tests
+{
+    public class TestAsNoTrackingHashset
+    {
+        private readonly ITestOutputHelper _output;
+
+        public TestAsNoTrackingHashset(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+
+        [Fact]
+        public void TestQueryPerformanceWithHashsetsOk()
+        {
+            //SETUP
+            int numBooks = 100;
+            var options = SqliteInMemory.CreateOptions<BookDbContext>();
+            using (var context = new BookDbContext(options))
+            {
+                context.Database.EnsureCreated();
+                context.SeedDatabaseDummyBooks(numBooks);
+            }
+            //ATTEMPT
+            using (var context = new BookDbContext(options))
+            using (new TimeThings(_output, "Normal"))
+            {
+                var books = context.Books
+                    .Include(x => x.Reviews)
+                    .Include(x => x.AuthorsLink).ThenInclude(x => x.Author)
+                    .ToList();
+            }
+            using (var context = new BookDbContext(options))
+            using (new TimeThings(_output, "AsNoTracking"))
+            {
+                var books = context.Books
+                    .AsNoTracking()
+                    .Include(x => x.Reviews)
+                    .Include(x => x.AuthorsLink).ThenInclude(x => x.Author)
+                    .ToList();
+            }
+            using (var context = new BookDbContext(options))
+            using (new TimeThings(_output, "AsNoTrackingWithIdentityResolution"))
+            {
+                var books = context.Books
+                    .AsNoTrackingWithIdentityResolution()
+                    .Include(x => x.Reviews)
+                    .Include(x => x.AuthorsLink).ThenInclude(x => x.Author)
+                    .ToList();
+            }
+
+            using (var context = new BookDbContext(options))
+            using (new TimeThings(_output, "Normal2"))
+            {
+                var books = context.Books
+                    .Include(x => x.Reviews)
+                    .Include(x => x.AuthorsLink).ThenInclude(x => x.Author)
+                    .ToList();
+            }
+
+            using (var context = new BookDbContext(options))
+            using (new TimeThings(_output, "AsNoTracking2"))
+            {
+                var books = context.Books
+                    .AsNoTracking()
+                    .Include(x => x.Reviews)
+                    .Include(x => x.AuthorsLink).ThenInclude(x => x.Author)
+                    .ToList();
+            }
+
+            using (var context = new BookDbContext(options))
+            using (new TimeThings(_output, "AsNoTrackingWithIdentityResolution2"))
+            {
+                var books = context.Books
+                    .AsNoTrackingWithIdentityResolution()
+                    .Include(x => x.Reviews)
+                    .Include(x => x.AuthorsLink).ThenInclude(x => x.Author)
+                    .ToList();
+            }
+        }
+    }
+}
