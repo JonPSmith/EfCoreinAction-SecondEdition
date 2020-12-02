428d4d96061d5a9fe4e210ed432f275984865017
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-12-02 10:19:04 +0000

    Rnamed SQL (cached) to  SQL (+cache)

--- BookApp.UI/Services/MenuBuilder.cs
+++ BookApp.UI/Services/MenuBuilder.cs
@@ -23,13 +23,13 @@ namespace BookApp.UI.Services
                 new MenuData("DefaultSql", "SQL (LINQ)"),
                 new MenuData("UdfsSql", "SQL (+UDFs)"),
                 new MenuData("DapperSql", "SQL (Dapper)"),
-                new MenuData("CachedSql", "SQL (cached)")
+                new MenuData("CachedSql", "SQL (+cache)")
             },
             [BookAppMenuSettings.SqlAndCosmos] = new List<MenuData>
             {
                 new MenuData("CosmosEf", "Cosmos (EF)"),
                 new MenuData("CosmosDirect", "Cosmos (Direct)"),
-                new MenuData("CachedSql", "SQL (cached)"),
+                new MenuData("CachedSql", "SQL (+cache)"),
                 new MenuData("DapperSql", "SQL (Dapper)")
             },
             [BookAppMenuSettings.CosmosOnly] = new List<MenuData>
