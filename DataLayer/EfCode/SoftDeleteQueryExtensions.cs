34600ab56be951e0f9f627fdad81bb46bd55630f
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-03-01 09:58:59 +0000

    Added comments on the index

--- DataLayer/EfCode/SoftDeleteQueryExtensions.cs
+++ DataLayer/EfCode/SoftDeleteQueryExtensions.cs
@@ -27,28 +27,31 @@ namespace DataLayer.EfCode
             var filter = methodToCall                             //#G
                 .Invoke(null, new object[] { userIdProvider });   //#G
             entityData.SetQueryFilter((LambdaExpression)filter);  //#H 
-            if (queryFilterType == MyQueryFilterTypes.SoftDelete)
-                entityData.AddIndex(entityData.FindProperty(nameof(ISoftDelete.SoftDeleted)));
-            if (queryFilterType == MyQueryFilterTypes.UserId)
-                entityData.AddIndex(entityData.FindProperty(nameof(IUserId.UserId)));
+            
+            if (queryFilterType == MyQueryFilterTypes.SoftDelete) //#I
+                entityData.AddIndex(entityData.FindProperty(      //#I
+                    nameof(ISoftDelete.SoftDeleted)));            //#I
+            if (queryFilterType == MyQueryFilterTypes.UserId)     //#J
+                entityData.AddIndex(entityData.FindProperty(      //#J
+                    nameof(IUserId.UserId)));                     //#J
         }
 
-        private static LambdaExpression GetUserIdFilter<TEntity>(     //#I
-            IUserId userIdProvider)                                   //#I
-            where TEntity : class, IUserId                            //#I
-        {                                                             //#I
-            Expression<Func<TEntity, bool>> filter =                  //#I
-                x => x.UserId == userIdProvider.UserId;               //#I
-            return filter;                                            //#I
-        }                                                             //#I
+        private static LambdaExpression GetUserIdFilter<TEntity>(     //#K
+            IUserId userIdProvider)                                   //#K
+            where TEntity : class, IUserId                            //#K
+        {                                                             //#K
+            Expression<Func<TEntity, bool>> filter =                  //#K
+                x => x.UserId == userIdProvider.UserId;               //#K
+            return filter;                                            //#K
+        }                                                             //#K
 
-        private static LambdaExpression GetSoftDeleteFilter<TEntity>( //#J
-            IUserId userIdProvider)                                   //#J
-            where TEntity : class, ISoftDelete                        //#J
-        {                                                             //#J
-            Expression<Func<TEntity, bool>> filter =                  //#J
-                x => !x.SoftDeleted;                                  //#J
-            return filter;                                            //#J
+        private static LambdaExpression GetSoftDeleteFilter<TEntity>( //#L
+            IUserId userIdProvider)                                   //#L
+            where TEntity : class, ISoftDelete                        //#L
+        {                                                             //#L
+            Expression<Func<TEntity, bool>> filter =                  //#L
+                x => !x.SoftDeleted;                                  //#L
+            return filter;                                            //#L
         }
     }
     /*******************************************************
@@ -58,10 +61,12 @@ namespace DataLayer.EfCode
     #D First parameter comes from EF Core and allows you to add a query filter
     #E Second parameter allows you to pick which type of query filter to add
     #F Third, optional property holds a copy of the current DbContext instance so that the UserId will be the current one
-    #G Create the correctly typed method to create the where LINQ expression to use in the Query Filter
+    #G Create the correctly typed method to create the Where LINQ expression to use in the Query Filter
     #H Uses the filter returns by the created type method in the SetQueryFilter method 
-    #I Creates a query that is only true if the _userId matches the UserID in the entity class
-    #J Creates a query that is only true if the SoftDeleted property is false
+    #I Add an index on the SoftDeleted property for better performance
+    #J Add an index on the UserId property for better performance
+    #K Creates a query that is only true if the _userId matches the UserID in the entity class
+    #L Creates a query that is only true if the SoftDeleted property is false
      ********************************************************/
 
 }
